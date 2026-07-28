969a6e74673330e4591e2f4fd49ad00ceac65f55
Abhishek Singh Sahil <abhishek2023gdsc@gmail.com>
2026-07-28 13:33:01 +0530

    Update project

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://anand-vihar-ifn2.onrender.com/api";
+// https://anand-vihar-ifn2.onrender.com
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
